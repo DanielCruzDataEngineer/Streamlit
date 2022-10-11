226aae7cd9c4124c6005510e700a1f7ed202396e
DanielCruzDataEngineer <danielcruz.alu.lmb@gmail.com>
2022-10-11 19:34:39 -0300

    ok

--- web_app_v3.py
+++ web_app_v3.py
@@ -15,7 +15,7 @@ def local_css(file_name):
 st.sidebar.subheader("""Welcome to Zorro API Dashs(BDRs)""")
 selected_stock = st.sidebar.text_input("Enter a valid stock ticker...", "GOOG")
 button_clicked = st.sidebar.button("GO")
-start_date = st.date_input('Start date : ')
+start_date = st.date_input('Start date : ',"2022-07-01")
 if button_clicked == "GO":
 
     main()
