8edff9d452e3b0ae9691c04b839a7b6e4e4ecc7a
DanielCruzDataEngineer <danielcruz.alu.lmb@gmail.com>
2022-10-11 19:49:35 -0300

    ok

--- web_app_v3.py
+++ web_app_v3.py
@@ -126,5 +126,7 @@ def main():
         display_sustainability = (stock_data.sustainability)
     if display_sustainability.empty == True:
         st.write("No data available at the moment")
+    else:
+        st.write(display_sustainability)
 if __name__ == "__main__":
     main()
